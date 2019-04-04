ccf760fe38b580b4e9d6175153bce80f8fa3c638
MangoZT <zhuhuaitian@163.com>
2019-04-04 18:01:08 +0800

    添加@babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const babelConfig = {
         useBuiltIns: "entry",
         corejs: 2
     }], "@babel/preset-react"],
-    plugins: ["@babel/plugin-syntax-dynamic-import",['@babel/plugin-transform-runtime']]
+    plugins: ["@babel/plugin-syntax-dynamic-import",'@babel/plugin-transform-runtime','@babel/plugin-proposal-class-properties']
 }
 
 module.exports = babelConfig;
