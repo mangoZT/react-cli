0f0e76c3a44fc40a72a0a20007941061e22c301f
朱虎 <hu.zhu@weimob.com>
2020-07-08 03:30:31 +0800

    添加node代理转发

--- server/express.js
+++ server/express.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+var path = require("path");
 var app = express();
 
-app.get('/dist*', function (req, res) {
-   res.sendFile( path.join(__dirname , "../" + req.url));
-})
-app.use(function (req, res) {
-	res.sendFile(path.join( __dirname , "../dist/" + "index.html" ));
-}) 
- 
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-})
+app.get("/dist*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../" + req.url));
+});
+app.use(
+  "/api",
+  proxy.createProxyMiddleware({
+    target: "http://10.119.168.87:4000",
+    changeOrigin: true
+  })
+);
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../dist/" + "index.html"));
+});
+
+var server = app.listen(8081, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("应用实例，访问地址为 http://%s:%s", host, port);
+});
